1eed1507474dc3c0a7d6e4a4012165e12f6adaa9
ramindu90 <ramindus@wso2.com>
2024-04-11 15:57:40 +0530

    add changes to java install script

--- install-java.sh
+++ install-java.sh
@@ -2,7 +2,7 @@ sudo apt install default-jdk -y
 sudo update-alternatives --config java 
 java_home=$(sudo update-alternatives --config java | grep 'java' | awk '{print $3}')
 # Append JAVA_HOME setting to /etc/profile
-echo "export JAVA_HOME=$java_home" | sudo tee -a /etc/profile > /dev/null
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" | sudo tee -a /etc/profile > /dev/null
 # Append JAVA_HOME/bin to the PATH variable in /etc/profile
 echo "export PATH=\$JAVA_HOME/bin:\$PATH" | sudo tee -a /etc/profile > /dev/null
 # Source the /etc/profile to apply changes
